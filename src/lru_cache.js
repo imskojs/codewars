d6fba2aa3d44fb6eae1da6015214de848e272c79
Seunghoon Ko <imskojs@gmail.com>
2016-09-29 11:46:31 +0900

    LCU cache Test

--- src/lru_cache.js
+++ src/lru_cache.js
@@ -1,2 +1,5 @@
 //  When the cache is full, the algorithm must choose which items to 
 // discard to make room for the new ones.
+
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+
