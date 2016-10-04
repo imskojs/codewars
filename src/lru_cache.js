9de05ef2b4aa37fca13f1ef7efaf70c6a27c3ed6
Seunghoon Ko <imskojs@gmail.com>
2016-10-05 08:49:57 +0900

    kids

--- src/lru_cache.js
+++ src/lru_cache.js
@@ -5,12 +5,48 @@
 
 
   
-let LRUCache = class LRUCache {
+class LRUCache {
   constructor(capacity, init){
-    // Object.defineProperty(this, '')
-  }
+    this.pairs = {};
+    this.capacity = capacity;
 
-}
+    Object.defineProperty(this, 'size', {
+      enumerable: false,
+      configurable: false,
+      writable: true,
+      value: 0
+    });
+
+    if(init){
+      for(let key in init){
+        Object.defineProperty(this, key, {
+          enumerable: true,
+          configurable: true,
+          // writable: true,
+          // value: init[key],
+          get: function(){
+            return this.pairs[key];
+          },
+          set: function(value){
+            if(this.pairs[key] === undefined){
+              ++this.size;
+            }
+            this.pairs[key] = value;
+          }
+        });
+        console.log("key :::\n", init[key]);
+        this[key] = init[key];
+      }
+    }
+
+  } //ctor
+
+} // LRU
+
+let x = new LRUCache(4, {a: 1});
+console.log("x :::\n", x);
+console.log("x.size :::\n", x.size);
+console.log("x.a :::\n", x.a);
 
 
 
@@ -18,3 +54,5 @@ let LRUCache = class LRUCache {
 
 
 
+
+module.exports = LRUCache;
